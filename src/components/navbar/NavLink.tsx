a7d114b4f5f304e51cf0b367eb377edfa124b34a
phmudau022 <phumulamudau259@gmail.com>
2024-05-23 18:36:16 +0200

    update

--- src/components/navbar/NavLink.tsx
+++ src/components/navbar/NavLink.tsx
@@ -1,12 +1,35 @@
+'use client'
 import Link from "next/link";
-import React from "react";
+import React, {useState, useEffect} from "react";
+import { usePathname } from "next/navigation";
 
 export const NavLink = (props: NavLInkProps) => {
   const { title, url } = props;
+  const [active, setActive] = useState<boolean>(false)
+
+  const pathName = usePathname();
+  console.log(pathName)
+  const pathLocation =  pathName.split('/')[1];
+
+  console.log(pathLocation, url)
+
+  useEffect(() => {
+
+    if(pathLocation === url){
+      setActive(true)
+    }
+    else{
+      setActive(false)
+    }
+  }, [url, pathLocation])
+
 
   return (
     <Link href={`/${url}`}
-    className="text-gray-900 hover:text-gray-700 px-3 py-2 rounded-md text-sm font-medium"
+    className={active ? "text-gray-900 underline hover:text-gray-700 px-3 py-2 rounded-md text-sm font-medium" : 
+    "text-gray-900 hover:text-gray-700 px-3 no-underline py-2 rounded-md text-sm font-medium"
+
+    }
     >
         {title}
     </Link>
