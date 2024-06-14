1bcfa2c273762a284b4c05f4f4db2b00e14bde06
phmudau022 <phumulamudau259@gmail.com>
2024-06-14 12:56:08 +0200

    responsive update on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import pic from "@/assets/pic2.jpg";
 const Page = () => {
   return (
     <div className="w-screen h-screen relative">
-      <div className="h-[60vh] md:h-[70vh] lg:h-[80vh] xl:h-[90vh]">
+      <div className="h-screen">
         <Image
           height={1000}
           width={1000}
@@ -16,13 +16,13 @@ const Page = () => {
           src={pic}
           className="w-full h-full object-cover"
         />
-        <div className="absolute top-0 left-0 bg-black/80 w-full h-full flex items-center ">
+        <div className="absolute top-0 left-0 bg-black/80 w-full h-full flex items-center justify-center">
           <div className="w-11/12 md:w-4/5 lg:w-3/5 mx-auto flex flex-col gap-4 md:gap-6 lg:gap-8 items-center text-center p-4">
             <h5 className="text-white text-xs md:text-sm lg:text-base mb-2">
               DONATE TO CONTRIBUTE
             </h5>
             <h1 className="text-2xl md:text-3xl lg:text-4xl text-white font-bold mb-4">
-              LET&apos;S BUILD A BETTER WORLD TOGETHER 
+              LET&apos;S BUILD A BETTER WORLD TOGETHER
             </h1>
             <p className="text-white text-sm md:text-base lg:text-lg">
               Together, we can make a lasting impact on the world around us. By
